e9d7b0d98ee69fb291ab585c20868a8205c7cf76
Elliot <Elliot@RA>
2023-12-08 10:46:08 -0700

    Added a picsum random photo

--- diceroller-service/public/login.js
+++ diceroller-service/public/login.js
@@ -21,3 +21,14 @@ function register() {
 
     // TODO: send to websocket
 }
+
+function displayRandomImage() {
+    const random = Math.floor(Math.random() * 100);
+    fetch(`https://picsum.photos/v2/list?page=${random}&limit=1`)
+        .then((response) => response.json())
+        .then((data) => {
+            document.getElementById('random-image').src = `https://picsum.photos/id/${data[0].id}/500/300?grayscale`;
+        });
+}
+
+displayRandomImage();
