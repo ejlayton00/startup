7f4757c15de327fc52f03e3faeef15087fe1bf96
Elliot <Elliot@RA>
2023-12-09 12:54:41 -0700

    Replaced empty calls in index.js to use mongodb

--- diceroller-login/index.js
+++ diceroller-login/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const DB = require('./database.js');
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
@@ -19,20 +20,24 @@ app.listen(port, () => {
 });
 
 // Application specific
-apiRouter.get('/loadsets', (_req, res) => {
+apiRouter.get('/loadsets', async (_req, res) => {
+    const dicesets = await DB.getDicesets();
     res.send(dicesets);
 })
 
-apiRouter.post('/saveset', (req, res) => {
+apiRouter.post('/saveset', async (req, res) => {
+    DB.addDiceset(req.body);
+    const dicesets = await DB.getDicesets();
     dicesets = updateDicesets(req.body, dicesets);
     res.send(dicesets);
 })
 
-let dicesets = [];
-function updateDicesets(newDiceSet, dicesets) {
-    dicesets.push(newDiceSet);
-    if (dicesets.length > 10) {
-        dicesets.length = 10;
-    }
-    return dicesets;
-}
+// Unneeded code due to now having a database.
+// let dicesets = [];
+// function updateDicesets(newDiceSet, dicesets) {
+//     dicesets.push(newDiceSet);
+//     if (dicesets.length > 10) {
+//         dicesets.length = 10;
+//     }
+//     return dicesets;
+// }
