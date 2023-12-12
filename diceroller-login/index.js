23fad5b915e6f50f212ed909aab75964e08b0aea
Elliot <Elliot@RA>
2023-12-12 11:33:16 -0700

    More "email" -> "username"

--- diceroller-login/index.js
+++ diceroller-login/index.js
@@ -23,17 +23,17 @@ app.use(`/api`, apiRouter);
 
 // Auth
 apiRouter.post('/auth/create', async (req, res) => {
-    if (await DB.getUser(req.body.email)) {
+    if (await DB.getUser(req.body.username)) {
         res.status(409).send({ msg: 'Existing user' });
     } else {
-        const user = await DB.createUser(req.body.email, req.body.password);
+        const user = await DB.createUser(req.body.username, req.body.password);
         setAuthCookie(res, user.token);
         res.send({ id: user._id, });
     }
 });
 
 apiRouter.post('/auth/login', async (req, res) => {
-    const user = await DB.getUser(req.body.email);
+    const user = await DB.getUser(req.body.username);
     if (user) {
         if (await bcrypt.compare(req.body.password, user.password)) {
             setAuthCookie(res, user.token);
@@ -49,11 +49,11 @@ apiRouter.delete('/auth/logout', (_req, res) => {
     res.status(204).end();
 });
 
-apiRouter.get('/user/:email', async (req, res) => {
-    const user = await DB.getUser(req.params.email);
+apiRouter.get('/user/:username', async (req, res) => {
+    const user = await DB.getUser(req.params.username);
     if (user) {
         const token = req?.cookies.token;
-        res.send({ email: user.email, authenticated: token === user.token });
+        res.send({ username: user.username, authenticated: token === user.token });
         return;
     }
     res.status(404).send({ msg: 'Unknown' });
